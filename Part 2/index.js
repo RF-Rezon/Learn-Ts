4d74fa53dae32603caaca0dcbfc6aadafd337a5b
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-05-04 17:57:05 +0600

    Object with Literal Types, Optional Types

--- Part 2/index.js
+++ Part 2/index.js
@@ -1,19 +1,6 @@
 "use strict";
-// Premitive Data Types
-// string
-let x = "Hi";
-console.log(x);
-// number
-let a = 20;
-console.log(a);
-// boolean
-// undefined
-// null
-// Non premitive Types
-// Array
-let arr = [30, 40];
-console.log(arr);
-// Tupple
-let tupp = [30, "Rahim"];
-console.log(tupp[0]);
-// Object
+let Person = {
+    name: "Rejwan",
+    age: 24,
+    height: 5.7,
+};
--- Part 2/index.ts
+++ Part 2/index.ts
@@ -1,26 +1,10 @@
-// Premitive Data Types
-
-// string
-
-let x: string = "Hi";
-console.log(x);
-
-// number
-
-let a: number = 20;
-console.log(a);
-
-// boolean
-// undefined
-// null
-
-// Non premitive Types
-
-// Array
-let arr: number[] = [30, 40];
-console.log(arr);
-// Tupple
-let tupp: [number, string] = [30, "Rahim"];
-console.log(tupp);
-
-// Object
+let Person: {
+  name: "Rejwan"; // Literal Types
+  age: number;
+  height: number;
+  religion?: boolean; // Optional Types
+} = {
+  name: "Rejwan",
+  age: 24,
+  height: 5.7,
+};
