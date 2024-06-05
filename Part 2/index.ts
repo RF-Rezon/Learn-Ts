00df5c9ad096f18f141a5e6c57aa0bc0954440bb
Rezwan Faysal <rezwanfaysalrezon@gmail.com>
2024-06-05 23:31:11 +0600

    start classes . start using bun.

--- Part 2/index.ts
+++ Part 2/index.ts
@@ -1,16 +1,31 @@
 {
-  let carFunc = <X, Y extends keyof X>(a: X, b: Y) => {
-    let totalModel = a[b];
+  class Device {
+    // name: string;
+    // work: string;
+    // color: string;
+    // brand: string;
 
-    console.log(totalModel);
-  };
+    constructor(
+      public name: string,
+      public work: string,
+      public color: string,
+      public brand: string
+    ) {
+      // this.name = name;
+      // this.work = work;
+      // this.color = color;
+      // this.brand = brand;
+    }
 
-  let car = {
-    color: "White",
-    name: "Lambourghini",
-    model: "Sian",
-    Price: 3,
-  };
+    about() {
+      console.log(
+        `This is a ${this.name}. It does ${this.work}. It is ${this.color} colored, and ${this.brand} branded.`
+      );
+    }
+  }
 
-  carFunc(car, "color");
+  const mobile = new Device("Mobile", "Connect people", "black", "Sumsang");
+
+  console.log(mobile.brand);
+  mobile.about();
 }
